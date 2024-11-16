ce6af1758947384fef9f9ef46298d345e7bd4a56
Yueyang Ding <dingyeuyang@gmail.com>
2024-11-16 14:42:16 -0500

    Initial Commit

---
+++ YueyangWeather/YueyangWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  YueyangWeatherApp.swift
+//  YueyangWeather
+//
+//  Created by Yueyang Ding on 2024-11-16.
+//
+
+import SwiftUI
+
+@main
+struct YueyangWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
